73c204e20ec1a2893fd9f0cdeeca8490cdb14b02
faraplay <faraplay@outlook.com>
2021-04-15 22:51:30 +0100

    add property view to allow changing properties

---
+++ ImasArchiveApp/IntToStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ImasArchiveApp
+{
+    class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (int.TryParse((string)value, out int result))
+                return result;
+            else
+                return null;
+        }
+    }
+}
